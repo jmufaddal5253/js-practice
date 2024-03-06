d4ae4105eebf0b946c88fc789372adb19d6c513d
jMufaddal <68770684+jmufaddal5253@users.noreply.github.com>
2024-03-06 07:02:36 +0000

    datatypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 14789
+let accountEmail = "hello@gmail.com"
+var accountPass = "123654"
+accountCity = "Jaipur"
+
+console.log(accountID)
+console.table([accountID, accountEmail, accountPass, accountCity])
