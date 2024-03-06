74a99bdc1d66772562c6a23e34b30147a19af415
jMufaddal <68770684+jmufaddal5253@users.noreply.github.com>
2024-03-06 07:27:31 +0000

    added

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let score = "33"
+
+let convertToNumber = Number(score)
+
+console.log(score)
+console.log(typeof (score))// first it was string
+console.log(typeof (convertToNumber))// now it has been converted to number
+
+
+let signedIn = 0
+
+let booleanSignedIn = Boolean(signedIn)
+console.log(booleanSignedIn)
